421ddda93f065abc5b6db4ef2f4b050cdcd0c50c
angel mondragon <amondrave54@gmail.com>
2020-10-20 22:26:11 -0500

    version pre produción

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
-    <link rel="shortcut icon" href="images/servitec_ico.ico">
+    <link rel="shortcut icon" href="images/S_imagotipo.ico">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="css/main.css" />
